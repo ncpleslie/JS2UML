b499d3aa49186bed204f131b152da26af5724fbd
Nick Leslie <ncpleslie@gmail.com>
2020-08-26 22:44:41 +1200

    Added check to prevent error on bad args

--- src/command_line.py
+++ src/command_line.py
@@ -30,10 +30,8 @@ class CommandLine(Cmd):
 
     def do_setup(self, arg):
         "Setup allows you initialise the program and set the configurations "\
-            "you want. These include default output directory, "\
-            "default output file type, default output name."\
-            "Setup will even allow you to set "\
-            "a MongoDB as your prefered backup location\n\n"\
+            "you want. These include default input directory, "\
+            "default output file type, default output name.\n\n"\
             "Expected use: \n"\
             "`setup -f <default filename or directory> -o <default output>"\
             "-t <default filetype>` "\
